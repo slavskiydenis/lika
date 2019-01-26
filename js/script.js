7d38cf5191d6955aed8e1935303de4351b0ce431
Slavskiy Denis <slavskiydenis@gmail.com>
2019-01-26 17:23:42 +0700

    #2

--- js/script.js
+++
@@ -1,8 +0,0 @@
-/* Спойлер */
-$(document).ready(function() {
-    $('.spoiler').click(function() {
-        id='#answer'+$(this).attr("id").substr(2);
-        $(id).slideToggle('normal');
-        return false;
-    });
-});
